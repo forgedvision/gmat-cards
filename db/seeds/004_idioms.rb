b012bd86aeb1749b1e4b01d430300803405da269
forgedvision <forgedvision@gmail.com>
2013-11-26 02:03:31 +0000

    idioms and various UI fixes to help with cards with no meaning

---
+++ db/seeds/004_idioms.rb
@@ -0,0 +1,159 @@
+Group.create!(
+  name: 'idioms'
+)
+Card.create!(
+  group_id: '004',
+  term: 'As X is To Y',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Consider X Y',
+  meaning: '(not: Consider X to be Y)',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Contrast X With Y',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Declare X Y(not: Declare X to be Y)',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Different From',
+  meaning: '(not: Different Than)',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Enough X That Y',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Estimate To Be',
+  meaning: '(not: Estimate At or Estimate As)',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Far Away From (physical distance)',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Far From (degree)',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'From X to Y',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Identical With',
+  meaning: '(not: Identical To)',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Independent Of',
+  meaning: '(= regardless) vs. ',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Independent From',
+  meaning: '(= X is separate from Y)',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'In Contrast To',
+  meaning: '(not: In Contrast With)',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Just As X…So Y',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Native To',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'A Native Of',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'No Less X Than Y',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Not Only X…But Also Y',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Not So Much X As Y',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Not X But Rather Y',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Rate For (financial)',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Rate Of (not financial)',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Regard As',
+  meaning: '(not: Regard to be)',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'So As Not To Be',
+  meaning: '',
+  done: 'false'
+)
+Card.create!(
+  group_id: '004',
+  term: 'Try To',
+  meaning: '(not: Try And)',
+  done: 'false'
+)
